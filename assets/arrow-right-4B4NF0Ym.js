5517594f8ba441ef22f2cd9e08df9e95840d845d
adarmawan117 <adarmawan117@gmail.com>
2026-07-02 16:05:47 +0700

    Updates

--- assets/arrow-right-4B4NF0Ym.js
+++ assets/arrow-right-4B4NF0Ym.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-CXmDtJ2D.js";/**
+import{c as o}from"./index-5xuVVXbC.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
